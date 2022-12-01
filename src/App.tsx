19308a48125d5d9d47c6511ea7632b4988560239
Jonas Vargaski <jonasvargaski@hotmail.com>
2022-12-01 10:42:11 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Typography } from './packages/components/Typography'
 import { Button } from './packages/components/Button'
+import { OutlinedCheckButton } from './packages'
 
 export function App() {
   return (
     <div>
       <Button variant="secondary">AHUHAUHU</Button>
-      <Typography color="blue.200">ajashuas</Typography>
+      <Typography color="blue.200">ajashuas</Typography>]
+      <OutlinedCheckButton checked={false} onClick={() => null}>
+        ds
+      </OutlinedCheckButton>
     </div>
   )
 }
--- src/packages/components/OutlinedCheckButton/index.tsx
+++ src/packages/components/OutlinedCheckButton/index.tsx
@@ -1,47 +1,8 @@
-import { Stack, Typography } from '@mui/material'
-import { Container } from './styles'
+import {
+  OutlinedCheckButton as OutlinedCheckButtonComponent,
+  type OutlinedCheckButtonProps,
+} from './OutlinedCheckButton'
 
-type OutlinedCheckButtonProps = {
-  fullWidth?: boolean
-  title?: string
-  description?: string
-  onClick: (checked: boolean, reason: 'container' | 'actionButton') => void
-  checked: boolean
-  children: React.ReactNode
-  className?: string
-}
-
-const OutlinedCheckButton = ({
-  checked,
-  onClick,
-  title,
-  description,
-  children,
-  ...props
-}: OutlinedCheckButtonProps) => {
-  return (
-    <Container
-      {...props}
-      checked={checked}
-      onClick={(e) => {
-        e.stopPropagation()
-        onClick(!checked, 'container')
-      }}
-    >
-      {children ?? (
-        <>
-          <Stack gap="4px">
-            {title && <Typography variant="labelMedium">{title}</Typography>}
-            {description && (
-              <Typography color="gray.600" variant="paragraphSmall">
-                {description}
-              </Typography>
-            )}
-          </Stack>
-        </>
-      )}
-    </Container>
-  )
-}
+const OutlinedCheckButton: React.FC<OutlinedCheckButtonProps> = OutlinedCheckButtonComponent
 
 export { OutlinedCheckButton, type OutlinedCheckButtonProps }
